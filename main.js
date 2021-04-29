017142c0971a9e95ebc14a500c3455879e3757ba
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-04-29 16:24:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 Webcam.set({
-    width:350,
+    width:310,
     height:300,
-    image_format: 'png',
-    png_quality:90
-});
-
+    image_format : 'png',
+    png_quality:90,
+    constraints:{
+facingMode:"environment"
+    }
+});     
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
-
 function take_snapshot()
 {
-Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-});
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
 }
-console.log('ml5 version:',ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5HN9xUEC5/model.json',modelLoaded);
-function modelLoaded()
-{
-    console.log("modelLoaded");
-}
+console.log('ml5 version:' , ml5.version);
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 
 function check()
 {
-img=document.getElementById('captured_image');
-classifier.classify(img, gotResult);
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
 }
-function gotResult(error, results)
-{
+
+function gotResult(error, results) {
     if(error) {
-console.error(error);
+        console.error(error);
     }
     else{
         console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        document.getElementById("object_name").innerHTML = results[0].label;
     }
 }
-
+function modelLoaded()
+{
+    console.log("Model Loaded !");
+}
